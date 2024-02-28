62dca6290078a074f2c3cfa97221d37aedd00755
Sathwik-git <sathwikbodakuntla041@gmail.com>
2024-02-28 22:36:41 +0530

    added required files

---
+++ utils/techStack.ts
@@ -0,0 +1,16 @@
+const techStack = [
+  "reactjs",
+  "nodejs",
+  "nextjs",
+  "javaScript",
+  "python",
+  "mongodb",
+  "typescript",
+  "expressjs",
+  "firebase",
+  "tailwindcss",
+  "angular",
+  "redis",
+];
+
+export default techStack;
